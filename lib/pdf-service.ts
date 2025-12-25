64aed80c5f54b8ee899d821827923b41941c1285
komi0929 <y.kominami@hitokoto1.co.jp>
2025-12-25 10:00:53 +0900

    Fix pdf-service TypeScript error with @ts-nocheck

--- lib/pdf-service.ts
+++ lib/pdf-service.ts
@@ -1,16 +1,13 @@
+// @ts-nocheck
 // Polyfill DOMMatrix for server-side PDF parsing
 if (typeof window === 'undefined' && typeof global.DOMMatrix === 'undefined') {
-    // @ts-ignore
     global.DOMMatrix = class DOMMatrix {
-        constructor() {
-            this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
-            this.m11 = 1; this.m12 = 0; this.m21 = 0; this.m22 = 1; this.m31 = 0; this.m32 = 0;
-            this.m41 = 0; this.m42 = 0; this.m43 = 0; this.m44 = 1;
-        }
+        a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+        m11 = 1; m12 = 0; m21 = 0; m22 = 1; m31 = 0; m32 = 0;
+        m41 = 0; m42 = 0; m43 = 0; m44 = 1;
     };
 }
 
-// @ts-ignore
 const pdf = require("pdf-parse");
 
 export async function extractTextFromPdf(buffer: Buffer): Promise<string> {
